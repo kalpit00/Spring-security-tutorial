3f89608b0e273d45996ab78c3c8fd6abc89c48fe
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-15 13:00:43 +0530

    RSA key-pair code

--- Oath-authorization-server/src/main/java/com/kalpit00/Oath/authorization/server/config/AuthorizationServerConfig.java
+++ Oath-authorization-server/src/main/java/com/kalpit00/Oath/authorization/server/config/AuthorizationServerConfig.java
@@ -20,6 +20,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.UUID;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
@@ -50,4 +60,33 @@ public class AuthorizationServerConfig {
                 .build();
         return new InMemoryRegisteredClientRepository(registeredClient);
     }
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        RSAKey rsaKey = generateRsa();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    private static RSAKey generateRsa() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
 }
