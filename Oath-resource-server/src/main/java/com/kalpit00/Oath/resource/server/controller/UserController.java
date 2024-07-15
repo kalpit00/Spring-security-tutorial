8b1fb9166eb3da96abca97474456563fd496ab84
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-15 13:39:15 +0530

    add a User Controller to test resource server

---
+++ Oath-resource-server/src/main/java/com/kalpit00/Oath/resource/server/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.kalpit00.Oath.resource.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/api/users")
+    public String[] getUser() {
+        return new String[] {"Max", "John","James"};
+    }
+}
