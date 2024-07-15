1cc9397d090272851dfefaa47f05382cb07cc1a2
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-15 13:13:37 +0530

    bind custom authenticator to application

--- Oath-authorization-server/src/main/java/com/kalpit00/Oath/authorization/server/config/DefaultSecurityConfig.java
+++ Oath-authorization-server/src/main/java/com/kalpit00/Oath/authorization/server/config/DefaultSecurityConfig.java
@@ -1,14 +1,19 @@
 package com.kalpit00.Oath.authorization.server.config;
 
+import com.kalpit00.Oath.authorization.server.service.CustomAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class DefaultSecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -17,5 +22,11 @@ public class DefaultSecurityConfig {
                 )
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Autowired
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder
+                .authenticationProvider(customAuthenticationProvider);
     }
 }
