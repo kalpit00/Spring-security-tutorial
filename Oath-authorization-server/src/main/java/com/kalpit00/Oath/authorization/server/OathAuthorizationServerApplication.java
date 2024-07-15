f439fe9ce58390bd2fc123e218937dfb575ec3fb
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-15 12:35:35 +0530

    O-auth2.0 module created + setup .yml and inject dependencies

---
+++ Oath-authorization-server/src/main/java/com/kalpit00/Oath/authorization/server/OathAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.kalpit00.Oath.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OathAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OathAuthorizationServerApplication.class, args);
+	}
+
+}
