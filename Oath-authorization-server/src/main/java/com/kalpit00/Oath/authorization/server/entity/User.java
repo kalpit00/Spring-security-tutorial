a93df719dd4e0c2971cd9d111d3fd268612c8c1e
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-15 12:40:18 +0530

    User entity and User repository added

---
+++ Oath-authorization-server/src/main/java/com/kalpit00/Oath/authorization/server/entity/User.java
@@ -0,0 +1,22 @@
+package com.kalpit00.Oath.authorization.server.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(length = 60)
+    private String password;
+
+    private String role;
+    private boolean enabled = false;
+}
