54ed11aec440a3c9621062cf4500d6b83cea97a6
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-15 13:06:24 +0530

    default security configuration

---
+++ Oath-authorization-server/src/main/java/com/kalpit00/Oath/authorization/server/config/DefaultSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.kalpit00.Oath.authorization.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
