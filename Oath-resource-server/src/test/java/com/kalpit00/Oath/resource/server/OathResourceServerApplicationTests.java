54e0df5dbecbe74e59cfe13399cdc14bbc2eabb1
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-15 13:33:28 +0530

    Oath Resource server add

---
+++ Oath-resource-server/src/test/java/com/kalpit00/Oath/resource/server/OathResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalpit00.Oath.resource.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OathResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
